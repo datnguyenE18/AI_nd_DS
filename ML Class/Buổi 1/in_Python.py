ed613b29eb53e1732d58f7e5e33f20b5497c997d
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-07-26 20:41:36 +0700

    Machine Learning

---
+++ ML Class/Buổi 1/in_Python.py
@@ -0,0 +1,20 @@
+﻿import random
+import math
+
+d = 20 
+R = 3 
+tries = 1000000 
+inside = 0 
+x = 5 
+y = -3 
+lmt = d / 2
+
+for i in range(tries):
+    rand_x = random.uniform(-lmt, lmt) 
+    rand_y = random.uniform(-lmt, lmt) 
+    
+    if (math.sqrt((rand_x - x)**2 + (rand_y - y)**2) <= R):
+        inside += 1
+
+pi = (d**2 * inside / tries) / (R**2)
+print (pi) # KQ: 3.1329333333333333
